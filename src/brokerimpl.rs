14845e1ee45207343b773c99c10c0e15a759a04c
Václav Kubernát <sir.venceslas@gmail.com>
2026-04-23 15:26:31 +0200

    Add peers to TunnelNode captures

--- src/brokerimpl.rs
+++ src/brokerimpl.rs
@@ -927,9 +927,9 @@ impl BrokerImpl {
         };
         add_node(DIR_APP, Box::new(AppNode::new()));
         if config.tunnelling.enabled {
-            add_node(".app/tunnel", Box::new(TunnelNode::new()));
+            add_node(".app/tunnel", Box::new(TunnelNode::new(peers.clone())));
             if let Some(tsub_dir) = &config.tunnelling.tsub_dir {
-                add_node(tsub_dir, Box::new(TunnelNode::new()));
+                add_node(tsub_dir, Box::new(TunnelNode::new(peers.clone())));
             }
         }
         add_node(DIR_BROKER, Box::new(BrokerNode::new(peers.clone(), config.name.clone())));
